5756ef265051436d432b2867edf2ebcc2d302694
dominicjk <jkdominic752@gmail.com>
2024-04-27 14:31:05 -0600

    changed staged

--- src/Analytics.tsx
+++ src/Analytics.tsx
@@ -15,11 +15,6 @@ interface ElectricCarData {
   }
 
 const AnalyticsPage: React.FC = () => {
-    const data = [
-        { name: 'Page A', uv: 400, pv: 2400, amt: 2400 },
-        { name: 'Page B', uv: 300, pv: 1398, amt: 2210 },
-        // More data can be added here
-    ];
 
     const calculateChargingEfficiency = (carData: ElectricCarData): number => {
         // Calculate charging efficiency (km/kWh)
