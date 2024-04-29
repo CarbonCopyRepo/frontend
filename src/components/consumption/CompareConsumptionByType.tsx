4b19cc7a8b81b18ada781e4d8024c6131cb6f351
Tyler <>
2024-04-29 12:45:41 -0600

    finished consumption page

--- src/components/consumption/CompareConsumptionByType.tsx
+++ src/components/consumption/CompareConsumptionByType.tsx
@@ -17,8 +17,6 @@ export default function CompareEmissionsByType() {
       const consumptionData1 = await getYearlyConsumptionTypeBTU(selectedType1, selectedMile);
       const consumptionData2 = await getYearlyConsumptionTypeBTU(selectedType2, selectedMile);
 
-      console.log(consumptionData1);
-
       const formattedData1 = consumptionData1.data.map((d) => {
         return {
           year: d.year,
@@ -33,16 +31,19 @@ export default function CompareEmissionsByType() {
         };
       });
 
-      console.log(formattedData1);
-      console.log(formattedData2);
-
-      const formattedData = formattedData1.concat(formattedData2);
-
-      console.log(formattedData);
+      const formattedData = formattedData1.map((d) => {
+        console.log(formattedData2[d.year]);
+        const newPt = formattedData2.find((e) => e.year === d.year);
+        if (newPt) {
+          return {
+            year: d.year,
+            consumption1: d.consumption1,
+            consumption2: newPt.consumption2,
+          };
+        }
+      });
 
       setConsumption(formattedData);
-
-      console.log(consumption);
     };
 
     if (selectedType1 && selectedType2) getBTUModel();
@@ -51,29 +52,27 @@ export default function CompareEmissionsByType() {
   return (
     <div className="bg-white rounded">
       <h1>Compare Consumption by Vehicle Type</h1>
-      <div className="flex flex-row">
+      <div className="flex flex-row h-80 py-5 pl-2">
         <div className="w-1/3">
           <VehicleTypeDropdown selectedType={selectedType1} setSelectedType={setSelectedType1} />
           <VehicleTypeDropdown selectedType={selectedType2} setSelectedType={setSelectedType2} />
           <MileDropdown selectedMile={selectedMile} setSelectedMile={setSelectedMile} />
         </div>
         <div>
-          {consumption && consumption.length > 0 && (
-            <LineChart
-              width={730}
-              height={250}
-              data={consumption}
-              margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-            >
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="year" />
-              <YAxis />
-              <Tooltip />
-              <Legend />
-              <Line type="monotone" dataKey="consumption1" stroke="#8884d8" />
-              <Line type="monotone" dataKey="consumption2" />
-            </LineChart>
-          )}
+          <LineChart
+            width={730}
+            height={250}
+            data={consumption}
+            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="year" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="consumption1" stroke="#8884d8" />
+            <Line type="monotone" dataKey="consumption2" />
+          </LineChart>
         </div>
       </div>
     </div>
